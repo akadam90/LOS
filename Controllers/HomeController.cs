de7a324541bd28a6a040b2528d035d919dd8d430
akadam90 <kadamakshada90@yahoo.com>
2016-11-09 14:57:49 -0600

    First code commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LOS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Scenarios()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Surveys()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Calculator()
+        {
+            return View();
+        }
+    }
+}
